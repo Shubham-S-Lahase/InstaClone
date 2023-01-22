df64a8923c552fdb6aabe46c7d5f4eeeccf74109
Shubham <shubhamlahase@hmail.com>
2023-01-22 14:05:44 +0530

    Solved Navbar.css file import error

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
+import PostView from './components/PostView/PostView';
+import FormPage from './components/Form/Form';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route index element={ <LandingPage/> } />
+      <Route path='/Postview' element={ <PostView/> } />
+      <Route  path='FormPage' element={ <FormPage/> } />
       </Routes>
     </BrowserRouter>
   );
